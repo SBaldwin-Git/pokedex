73acb76d596e3b2f60568401dab2e5a1865cfb56
Sam Baldwin <sambaldwin92@gmail.com>
2023-12-13 20:47:24 +0000

    Added conditions fo rthe console log, and successsfully logged the bulbasaur name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App()
-{
+function App() {
   // fetch the data from the API link https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0 and store it in a state variable
   // create a function to handle the search input
   // create a function to handle the search button
@@ -11,22 +10,23 @@ function App()
   // create a function to handle the search by id
   // create a function to handle the search by generation
 
+  const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
   const [pokemon, setPokemon] = useState([]);
-  const [search, setSearch] = useState('');
-  const [type, setType] = useState('');
-  const [name, setName] = useState('');
-  const [id, setId] = useState('');
-  const [generation, setGeneration] = useState('');
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState("");
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [generation, setGeneration] = useState("");
 
-  useEffect(() =>
-  {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-      .then(response => response.json())
-      .then(data => setPokemon(data.results));
-  }
-    , []);
-  
-  
+  useEffect(() => {
+    console.log("Fetching data...");
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data.results));
+  }, []);
+
+  // console log the pokemon state variable
+  console.log(pokemon.length > 0 ? pokemon[0].name : "Loading...");
 
   return <div className="App"></div>;
 }
