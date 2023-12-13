cdb83c0db0c3dd6693c1a2b21a5a12def3eda6b9
Sam Baldwin <sambaldwin92@gmail.com>
2023-12-13 20:16:45 +0000

    Outlined basic functionality to add and also created use states and api fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+function App()
+{
+  // fetch the data from the API link https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0 and store it in a state variable
+  // create a function to handle the search input
+  // create a function to handle the search button
+  // create a function to handle the search by type
+  // create a function to handle the search by name
+  // create a function to handle the search by id
+  // create a function to handle the search by generation
+
+  const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('');
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
+  const [generation, setGeneration] = useState('');
+
+  useEffect(() =>
+  {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+      .then(response => response.json())
+      .then(data => setPokemon(data.results));
+  }
+    , []);
+  
+  
+
+  return <div className="App"></div>;
 }
 
 export default App;
