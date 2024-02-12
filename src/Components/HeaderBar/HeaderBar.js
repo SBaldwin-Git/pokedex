8c653187fd39784412bdb79c8ce4010b36b06354
Sam Baldwin <sambaldwin92@gmail.com>
2024-02-12 21:34:01 +0000

    Added search bar to header without functionality

--- src/Components/HeaderBar/HeaderBar.js
+++ src/Components/HeaderBar/HeaderBar.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  InputBase,
+  alpha,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
 import pokeballImage from "./my_pokeball_icon.png";
 import "@fontsource/press-start-2p";
 
@@ -34,6 +44,25 @@ function HeaderBar() {
     marginLeft: "1rem",
   };
 
+  const searchBoxStyle = {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "8px",
+    borderRadius: "4px",
+    backgroundColor: alpha("#FBFCF8", 0.1),
+  };
+
+  const searchIconStyle = {
+    marginRight: "8px",
+    color: alpha("#FBFCF8", 0.7),
+  };
+
+  const inputBaseStyle = {
+    color: "#FBFCF8",
+    width: "100%",
+  };
+
+
   return (
     <AppBar position="fixed" style={{ backgroundColor: "#232323" }}>
       <Toolbar style={headerStyle}>
@@ -55,6 +84,16 @@ function HeaderBar() {
           <Typography variant="h1" style={headerFontStyle}>
             My Pokédex
           </Typography>
+        </Box>
+        <Box sx={{ flexGrow: 1, maxWidth: "20%", marginLeft: "auto" }}>
+          <div style={searchBoxStyle}>
+            <SearchIcon style={searchIconStyle} />
+            <InputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+              style={inputBaseStyle}
+            />
+          </div>
         </Box>
       </Toolbar>
     </AppBar>
