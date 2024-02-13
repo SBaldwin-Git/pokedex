cfa706f1a61063aca3702dc3093af70719a66f9a
Sam Baldwin <sambaldwin92@gmail.com>
2024-02-13 16:22:46 +0000

    Added comments to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "@fontsource/press-start-2p";
 import { useState, useEffect } from "react";
 import { Container, Box } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Unstable_Grid2"; // Using Unstable_Grid2, ensure compatibility with the version of MUI
 import {
   createTheme,
   responsiveFontSizes,
@@ -12,10 +12,14 @@ import PokemonCard from "../PokemonCard/PokemonCard";
 import HeaderBar from "../HeaderBar/HeaderBar";
 
 function App() {
+  // Create and configure MUI theme
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
 
+  // API endpoint for Pokémon data
   const BASE_URL = "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135";
+
+  // State variables to manage Pokémon data, sprite URLs, loading status, and placeholder data
   const [pokemon, setPokemon] = useState([]);
   const [spriteUrls, setSpriteUrls] = useState([]);
   const [pokemonNumber, setPokemonNumber] = useState([]);
@@ -27,12 +31,13 @@ function App() {
     }))
   );
 
+  // Effect to fetch Pokémon data from the API and simulate additional API calls
   useEffect(() => {
     console.log("Fetching data...");
 
-    // Simulating API call delay
     const fetchData = async () => {
       try {
+        // Simulating API call delay
         const response = await fetch(BASE_URL);
         const data = await response.json();
 
@@ -43,6 +48,7 @@ function App() {
         const urls = data.results.map((p) => p.url);
         await fetchSpriteUrls(urls);
 
+        // Update state with fetched data
         setPokemon(data.results);
         setPlaceholderData([]); // Clear placeholder data
         setLoading(false);
@@ -52,10 +58,10 @@ function App() {
       }
     };
 
-   fetchData();
+    fetchData();
   }, []);
 
-  //Pokemon number fetching function
+  // Function to fetch Pokémon numbers based on URLs
   const fetchPokemonNumber = (number) => {
     const pokemonNumberPromises = number.map((number) =>
       fetch(number)
@@ -68,7 +74,7 @@ function App() {
       .catch((error) => console.error("Error fetching pokemon number:", error));
   };
 
-  //URL fetching function for sprites
+  // Function to fetch sprite URLs based on Pokémon URLs
   const fetchSpriteUrls = (urls) => {
     const spritePromises = urls.map((url) =>
       fetch(url)
@@ -81,6 +87,7 @@ function App() {
       .catch((error) => console.error("Error fetching sprite URLs:", error));
   };
 
+  // Render Pokémon cards using fetched data
   return (
     <ThemeProvider theme={theme}>
       <HeaderBar />
